d1f5a95c7f02f9a7d939ae5e9ead03f79bec15a4
Alejandro Celaya <alejandro.celaya@adidas.com>
2018-06-11 11:26:37 +0200

    Created base implementation for CommentBox and CommentList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
 
 export default class App extends React.Component {
   render() {
-    return <div>I'm the app component</div>
+    return (
+      <div>
+        <CommentBox />
+        <CommentList />
+      </div>
+    )
   }
 }
---
+++ src/components/CommentBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class CommentBox extends React.Component {
+  render() {
+    return <div>Comment Box</div>
+  }
+}
