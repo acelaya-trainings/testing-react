c8ff29d5e70a044c6a2fc5a0c6475d3a0917060f
Alejandro Celaya <alejandro.celaya@adidas.com>
2018-06-12 14:20:00 +0200

    Created CommentBox test

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import CommentBox from '../CommentBox';
+import { mount } from 'enzyme';
+
+describe('CommentBox', () => {
+  let wrapped;
+
+  beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+  });
+  afterEach(() => {
+    wrapped.unmount();
+  });
+
+  describe('structure', () => {
+    it('has a text area and a button', () => {
+      expect(wrapped.find('textarea').length).toEqual(1);
+      expect(wrapped.find('button').length).toEqual(1);
+    });
+  });
+
+  describe('the text area', () => {
+    beforeEach(() => {
+      wrapped.find('textarea').simulate('change', {
+        target: { value: 'foobar' }
+      });
+      wrapped.update();
+    });
+
+    it('allows text to be introduced', () => {
+      expect(wrapped.find('textarea').prop('value')).toEqual('foobar');
+    });
+
+    it('is cleaned when form is submitted', () => {
+      wrapped.find('form').simulate('submit');
+      wrapped.update();
+
+      expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+  });
+});
